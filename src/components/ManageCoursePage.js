f08469cb44eadf85afdef45814e8d05ab8856cbb
santoshsingh-94 <ausantoshsingh@gmail.com>
2019-12-10 17:29:12 +0530

    Added form+Reusable components+saving Data

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Prompt } from 'react-router-dom';
+import CourseForm from "../components/CourseForm";
+import * as courseApi from "../api/courseApi";
+
 const ManageCoursePage = props => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  function handleChange(event) {  //Destructuring
+    const updatedCourse = { ...course, [event.target.name]: event.target.value };
+    setCourse(updatedCourse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course);
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      <Prompt when={true} message="Are you sure you want to leave?" />
-      {props.match.params.slug}
+      {/*<Prompt when={true} message="Are you sure you want to leave?" />*/}
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
